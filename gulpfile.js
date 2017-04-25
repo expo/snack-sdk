4de1df1c5327ff6a83234991272d8dda7b855a46
Jesse Ruder <jesse@getexponent.com>
2017-04-25 03:03:13 +0000

    Add gulp publish
    fbshipit-source-id: 1b78b31

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const shell = require('gulp-shell');
 
 const buildTasks = require('./gulp/build-tasks');
 
@@ -8,3 +9,14 @@ gulp.task('build', gulp.parallel(tasks.babel, tasks.flow));
 gulp.task('watch', gulp.series(tasks.flow, tasks.babel, tasks.watchBabel));
 gulp.task('clean', tasks.clean);
 gulp.task('default', gulp.series('watch'));
+
+gulp.task(
+  'publish',
+  gulp.series(
+    tasks.clean,
+    tasks.flow,
+    tasks.babel,
+    tasks.caches,
+    shell.task(['npm publish'])
+  )
+);
