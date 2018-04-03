d0f3dfef7222610f6a5f91e8f718cfdbb807c74b
Miles Fertel <milesfertel@college.harvard.edu>
2018-04-03 22:59:35 +0000

    Just missing the peer inclusion of fetch and formdata
    fbshipit-source-id: c23a352

--- src/utils/sendFileUtils.js
+++ src/utils/sendFileUtils.js
@@ -1,8 +1,6 @@
 /* flow */
 
 var diff = require('diff');
-var fetch = require('node-fetch');
-var FormData = require('form-data');
 
 const getFileDiff = (oldCode: string, newCode: string) => {
   const patch = diff.createPatch('code', oldCode, newCode, '', '', {
