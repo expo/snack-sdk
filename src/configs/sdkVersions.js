dcff41055737d60f2a7c55fc47e5578bd10fcd93
TC Davis <thomas.c.davis@gmail.com>
2018-09-11 21:53:40 +0000

    Upgrade for SDK 30
    fbshipit-source-id: 58ed9d0

--- src/configs/sdkVersions.js
+++ src/configs/sdkVersions.js
@@ -16,9 +16,10 @@ export const versions = {
   '27.0.0': [],
   '28.0.0': [],
   '29.0.0': [],
+  '30.0.0': [],
 };
 
-export const defaultSDKVersion = '29.0.0';
+export const defaultSDKVersion = '30.0.0';
 
 export type Feature = $Keys<typeof minFeatureVersion>;
 export type SDKVersion = $Keys<typeof versions>;
