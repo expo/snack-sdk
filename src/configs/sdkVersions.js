145773245088d4b4a9c945b123bbc82a3dda3ba1
TC Davis <thomas.c.davis@gmail.com>
2019-09-20 01:12:42 +0000

    Upgrade snack for SDK 35
    fbshipit-source-id: 8ddbd73

--- src/configs/sdkVersions.js
+++ src/configs/sdkVersions.js
@@ -12,10 +12,9 @@ const minFeatureVersion = {
 
 // special casing of features that have been backported to particular SDK versions
 export const versions = {
-  '31.0.0': [],
-  '32.0.0': [],
   '33.0.0': [],
   '34.0.0': [],
+  '35.0.0': [],
 };
 
 export const defaultSDKVersion = '34.0.0';
@@ -24,8 +23,9 @@ export type Feature = $Keys<typeof minFeatureVersion>;
 export type SDKVersion = $Keys<typeof versions>;
 
 export const sdkSupportsFeature = (sdkVersion: SDKVersion, feature: Feature) => {
-  const versionsIncludeFeature = versions[sdkVersion] ? versions[sdkVersion].includes(feature) : false;
-  const result =
-    semver.gte(sdkVersion, minFeatureVersion[feature]) || versionsIncludeFeature
+  const versionsIncludeFeature = versions[sdkVersion]
+    ? versions[sdkVersion].includes(feature)
+    : false;
+  const result = semver.gte(sdkVersion, minFeatureVersion[feature]) || versionsIncludeFeature;
   return result;
 };
